9da424c0461fe5d888d1f9f4811cc103ccbe1f7c
qingqing625 <969548863@qq.com>
2020-06-07 10:14:43 +0800

    first

---
+++ src/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class demo01 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ("aaa");
+    }
+}
+ 
+export default demo01;
